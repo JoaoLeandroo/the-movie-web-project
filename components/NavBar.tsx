528ba79a33a1a4d4046848ff9083bd15d2fcae3f
joaoleandro <new.joaoleandro@gmail.com>
2024-02-04 14:07:48 -0300

    visual changes in input

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <header className="bg-[#585858a1] sticky top-0 shadow-xl">
       <Container>
-        <div className="h-9 sm:h-14 flex items-center justify-between">
+        <div className="h-9 sm:h-20 flex items-center justify-between">
           <h1 className="font-bold ">The Movies</h1>
 
           <div>
@@ -15,14 +15,23 @@ const NavBar = () => {
               className="
                 w-[300px]
                 px-3 
-                py-1 
-                rounded-md 
-                bg-[#F2F2F2] 
+                py-2 
+                rounded-l-full
+                bg-[#585858] 
                 outline-none
                 text-black
-                focus:border-[5px] border-green-300
+                border border-slate-500
+                focus:border-white
                 "
             />
+            <button className="
+                bg-[#585858]
+                px-3
+                py-2
+                rounded-r-full
+                border border-slate-500
+            "
+            >search</button>
           </div>
 
           <div>testes</div>
